5289c66aab15ad0367c6904c70af4c6ad1636849
mrdarksidetm <ajukr99901@gmail.com>
2026-03-22 12:26:09 +0530

    fix: add Flutter Maven repository to settings.gradle.kts for Codemagic builds

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -22,6 +22,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://storage.googleapis.com/download.flutter.io")
+        }
     }
 }
 
