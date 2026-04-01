844a1869df25c4d35bdd212b1cc153de07fbfc8b
mrdarksidetm <ajukr99901@gmail.com>
2026-04-01 08:59:33 +0530

    fix: Remove unused import in CategoriesPage and verify analysis

--- lib/features/settings/pages/categories_page.dart
+++ lib/features/settings/pages/categories_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
-import 'package:material_symbols_icons/symbols.dart';
 import '../../../core/database/providers.dart';
 import '../../../core/widgets/icon_picker.dart';
 import '../../../core/theme/color_extension.dart';
