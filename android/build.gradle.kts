6895f8c066bd1b7523d075d3a8c10854f4f02960
mrdarksidetm <ajukr99901@gmail.com>
2026-04-13 09:50:57 +0530

    fix: exhaustive artifact discovery and simplified Gradle configuration

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,22 +1,10 @@
-import com.android.build.gradle.BaseExtension
-
-subprojects {
-    afterEvaluate {
-        if ((project.plugins.hasPlugin("com.android.application") || project.plugins.hasPlugin("com.android.library"))) {    
-            val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
-            if (android.namespace == null) {
-                android.namespace = project.group.toString() + "." + project.name.replace("-", "_")
-            }
-            android.compileSdkVersion(36)
-        }
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
     }
-}
-subprojects {
-    project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-
-
