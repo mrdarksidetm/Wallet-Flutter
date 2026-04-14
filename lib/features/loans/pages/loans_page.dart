63548b55680f765e6ddcdd81aa5848cacd717879
mrdarksidetm <ajukr99901@gmail.com>
2026-04-14 22:41:49 +0530

    Fix: Add missing go_router import in loans_page.dart

--- lib/features/loans/pages/loans_page.dart
+++ lib/features/loans/pages/loans_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
 import 'package:material_symbols_icons/symbols.dart';
 import '../../../core/database/providers.dart';
