031264ab0b346f78020e3de9ebc0f34d3cd748de
Abhijeet Kumar <ajukr99901@gmail.com>
2026-08-20 21:34:07 +0530

    fix(toolchain): align Gradle wrapper to 8.11.1 and AGP to 8.7.3 for stable Flutter build

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.13.0")
+        classpath("com.android.tools.build:gradle:8.7.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.10")
     }
 }
