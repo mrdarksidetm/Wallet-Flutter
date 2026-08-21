d77a0635aff295e7e6d63dbf6dd72b1d9c3df6a6
Abhijeet Kumar <ajukr99901@gmail.com>
2026-08-21 09:07:31 +0530

    fix(android): add buildscript classpath to resolve BaseExtension and stream CI build failure logs

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,14 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.11.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.10")
+    }
+}
+
 allprojects {
     repositories {
         google()
